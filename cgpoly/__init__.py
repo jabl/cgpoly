b7afd7697a30ea4e7e16c3a022dcda5e6354a07c
Janne Blomqvist <Janne.Blomqvist@tkk.fi>
2009-04-08 17:41:36 +0300

    Fix imports

--- cgpoly/__init__.py
+++ cgpoly/__init__.py
@@ -1,1 +1,8 @@
+"""CGPOLY, a package for doing coarse-grained molecular dynamics simulations with GROMACS"""
+
+import cgpoly.topology
+import cgpoly.nonbonded
+import cgpoly.polymer
+import cgpoly.ljpot
+
 __all__ = ['topology', 'nonbonded', 'polymer', 'ljpot', 'params']
