8b0eaf12cdcce5dfd42acef1ce4b03798849c10a
Janne Blomqvist <Janne.Blomqvist@tkk.fi>
2011-12-28 18:51:55 +0200

    Fix time scaling in params

--- data/cgparams.py
+++ data/cgparams.py
@@ -43,13 +43,16 @@ temp = 570 # K
 
 # First, BPA-PC parameters
 
-# System scaling factor for reduced units
-rsigma = 4.41
+# System scaling factor for reduced units in Espresso [Å/sigma]
+_rsigma = 4.41
 # For normal units, for reduced units set sigma = rsigma
-sigma = 1.
+#sigma = 1.
 
 # Timestep, in Espresso with reduced units 0.001
-time_scaling = rsigma / math.sqrt(kb*temp / 73)
+#time_scaling = rsigma * math.sqrt(73 / (kb*temp))
+# Factor of 10 is due to rsigma being in units of Å=> 1e-10, and then 
+# a factor of 1e9 due to GROMACS time unit being ps.
+time_scaling = _rsigma * math.sqrt(73) / 10
 timestep = 0.001 * time_scaling
 # gamma = 1/tau
 tau_t = 1/0.5 * time_scaling
@@ -58,11 +61,12 @@ tau_t = 1/0.5 * time_scaling
 # Harmonic bonds, k and b, see section 4.2.1 in gromacs manual
 # Harmonic spring constant is taken from gaussian distr
 # 1/(stddev/sigma)**2
-cp_k = (sigma / 0.04) ** 2
-cp_r = 0.356 / sigma
+# std.dev is 0.04 Å = 0.004 nm
+cp_k = (1 / 0.004) ** 2
+cp_r = 0.356
 
 ip_k = cp_k
-ip_r = 0.293 / sigma
+ip_r = 0.293
 
 # Angle for IPC, very stiff 180 deg
 ipc_theta = 180.0
@@ -73,9 +77,9 @@ ipc_k = 0.1 * kb * temp * (180. / math.pi)**2
 # Non-bonded parameters
 # Note the value of c_sigma in Abrams MM 36 260 is incorrect, this is the
 # correct value!
-c_sigma = 0.349 / sigma
-p_sigma = 0.467 / sigma
-i_sigma = 0.519 / sigma
+c_sigma = 0.349
+p_sigma = 0.467
+i_sigma = 0.519
 
 
 # Wall interaction 10-4 LJ parameters
@@ -84,9 +88,9 @@ ev2kjmol = 96.48538 # 1 eV = 96.48538 kJ/mol
 cw_eps = 0.57 * ev2kjmol
 pw_eps = 0.50 * ev2kjmol
 iw_eps = 0.50 * ev2kjmol 
-cw_sigma = 3.12 / 10 / sigma
-pw_sigma = 3.02 / 10 / sigma
-iw_sigma = 3.43 / 10 / sigma
+cw_sigma = 3.12 / 10 # Factor of 10 due to Angstrom -> nm
+pw_sigma = 3.02 / 10
+iw_sigma = 3.43 / 10
 
 cutoff_wall = 3 # Multiply sigma with this to get the cutoff range
 
