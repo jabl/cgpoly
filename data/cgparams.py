6428f3020cc3c4202fc41de28c0f5e9eae678a01
Janne Blomqvist <Janne.Blomqvist@tkk.fi>
2011-12-30 11:44:12 +0200

    Yet one more time scaling fix

--- data/cgparams.py
+++ data/cgparams.py
@@ -48,14 +48,17 @@ _rsigma = 4.41
 # For normal units, for reduced units set sigma = rsigma
 #sigma = 1.
 
-# Timestep, in Espresso with reduced units 0.001
-#time_scaling = rsigma * math.sqrt(73 / (kb*temp))
-# Factor of 10 is due to rsigma being in units of Å=> 1e-10, and then 
-# a factor of 1e9 due to GROMACS time unit being ps.
-time_scaling = _rsigma * math.sqrt(73) / 10
-timestep = 0.001 * time_scaling
-# gamma = 1/tau
+# In the reduced unit BPA-PC system, the energy scale epsilon is kb*T.
+# Factor of 10 is due to rsigma being in units of Å=> 1e-10, and then
+# a factor of 1e12 due to GROMACS time unit being ps, and finally a
+# factor of 1e-3 due to unit conversion in the sqrt expression.
+time_scaling = _rsigma * math.sqrt(73 / (kb*temp)) / 10
+# gamma = 1/tau, Langevin dynamics friction coefficient is 0.5 in
+# reduced units.
 tau_t = 1/0.5 * time_scaling
+
+# Timestep, in Espresso with reduced units 0.001
+timestep = 0.001 * time_scaling
 
 
 # Harmonic bonds, k and b, see section 4.2.1 in gromacs manual
