a0fb43ec7dd07b888a13725062cf81ae1c233f59
Janne Blomqvist <blomqvist.janne@gmail.com>
2023-05-05 15:25:12 +0300

    Switch to Apache-2.0 license

--- cgpoly/nonbonded.py
+++ cgpoly/nonbonded.py
@@ -1,22 +1,7 @@
 #!/usr/bin/python
 
-# Copyright (C) 2009 Janne Blomqvist
-
-#  This file is part of cgpoly.
-
-#  cgpoly is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 3 of the License, or
-#  (at your option) any later version.
-
-#  cgpoly is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-
-#  You should have received a copy of the GNU General Public License
-#  along with cgpoly.  If not, see <http://www.gnu.org/licenses/>.
-
+# Copyright (C) 2009, 2023 Janne Blomqvist
+# SPDX-License-Identifier: Apache-2.0
 
 """Generate tables with the nonbonded interactions for the
 coarse-grained BPA-PC model
